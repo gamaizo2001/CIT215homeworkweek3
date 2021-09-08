d7c4de5f1e48c3a991492c027bfab8b117955919
gamaizo2001 <88865368+gamaizo2001@users.noreply.github.com>
2021-09-07 21:06:11 -0400

    commit first
    week 3: loops and conditionals

---
+++ homework3.js
@@ -0,0 +1,31 @@
+var animals = ["dog", "fish", "car", "shark", "cat", "dog"];
+
+for(let i = 0; i<animals.length; i++){
+    if(animals[i]=="cat"){
+        document.write("<p>I am a cat</p>");
+
+    }
+    else if(animals[i] =="dog"){
+        document.write("<p>borf borf</p>");
+    }
+    else{
+        document.write("<p>I'm an animal</p>");
+    }
+}
+function myFunction(){
+    var animals = ["dog", "fish", "car", "shark", "cat", "dog"];
+    var animals_new = animals.sort(() => Math.random() - 0.5)
+
+    for(let i = 0; i<animals_new.length; i++){
+        if(animals_new[i]=="cat"){
+            document.write("<p>I am a cat</p>");
+
+        }
+        else if(animals_new[i] =="dog"){
+            document.write("<p>borf borf</p>");
+        }
+        else{
+            document.write("<p>I'm an animal</p>");
+        }
+    }
+}
